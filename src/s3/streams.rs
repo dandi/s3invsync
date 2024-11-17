f4dbfd7a39f51968c3762ed9ded57ef37f413d6a
John T. Wodder II <git@varonathe.org>
2024-11-17 17:06:26 -0500

    Make stream yield dates one at a time

--- src/s3/streams.rs
+++ src/s3/streams.rs
@@ -5,6 +5,7 @@ use aws_sdk_s3::{operation::list_objects_v2::ListObjectsV2Error, types::CommonPr
 use aws_smithy_async::future::pagination_stream::PaginationStream;
 use aws_smithy_runtime_api::client::{orchestrator::HttpResponse, result::SdkError};
 use futures_util::Stream;
+use std::collections::VecDeque;
 use std::pin::Pin;
 use std::task::{ready, Context, Poll};
 use thiserror::Error;
@@ -17,6 +18,7 @@ pub(super) struct ListManifestDates {
     bucket: String,
     key_prefix: String,
     inner: Option<PaginationStream<Result<ListObjectsV2Output, InnerListError>>>,
+    results: VecDeque<DateHM>,
 }
 
 impl ListManifestDates {
@@ -34,40 +36,45 @@ impl ListManifestDates {
                     .into_paginator()
                     .send(),
             ),
+            results: VecDeque::new(),
         }
     }
 }
 
 impl Stream for ListManifestDates {
-    type Item = Result<Vec<DateHM>, ListObjectsError>;
+    type Item = Result<DateHM, ListObjectsError>;
 
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        let Some(inner) = self.inner.as_mut() else {
-            return None.into();
-        };
-        let Some(r) = ready!(inner.poll_next(cx)) else {
-            self.inner = None;
-            return None.into();
-        };
-        let page = match r {
-            Ok(page) => page,
-            Err(source) => {
-                self.inner = None;
-                return Some(Err(ListObjectsError {
-                    bucket: self.bucket.clone(),
-                    prefix: self.key_prefix.clone(),
-                    source,
-                }))
-                .into();
+        loop {
+            if let Some(d) = self.results.pop_front() {
+                return Some(Ok(d)).into();
             }
-        };
-        let dates = page
-            .common_prefixes
-            .unwrap_or_default()
-            .into_iter()
-            .filter_map(|CommonPrefix { prefix, .. }| prefix?.parse::<DateHM>().ok())
-            .collect::<Vec<_>>();
-        Some(Ok(dates)).into()
+            let Some(inner) = self.inner.as_mut() else {
+                return None.into();
+            };
+            let Some(r) = ready!(inner.poll_next(cx)) else {
+                self.inner = None;
+                return None.into();
+            };
+            let page = match r {
+                Ok(page) => page,
+                Err(source) => {
+                    self.inner = None;
+                    return Some(Err(ListObjectsError {
+                        bucket: self.bucket.clone(),
+                        prefix: self.key_prefix.clone(),
+                        source,
+                    }))
+                    .into();
+                }
+            };
+            self.results = page
+                .common_prefixes
+                .unwrap_or_default()
+                .into_iter()
+                .filter_map(|CommonPrefix { prefix, .. }| prefix?.parse::<DateHM>().ok())
+                .collect::<VecDeque<_>>();
+        }
     }
 }
 
