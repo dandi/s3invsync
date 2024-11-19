5776a2a6f449bc800b7bb615d1d630b4bd03a739
John T. Wodder II <git@varonathe.org>
2024-11-19 10:29:22 -0500

    Add `--path-filter` option

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use clap::Parser;
 use std::num::NonZeroUsize;
 use std::path::PathBuf;
 
-#[derive(Clone, Debug, Eq, Parser, PartialEq)]
+#[derive(Clone, Debug, Parser)]
 #[command(version)]
 struct Arguments {
     #[arg(short, long)]
@@ -22,6 +22,9 @@ struct Arguments {
 
     #[arg(short = 'O', long, default_value = "20")]
     object_jobs: NonZeroUsize,
+
+    #[arg(long)]
+    path_filter: Option<regex::Regex>,
 
     inventory_base: S3Location,
 
@@ -34,7 +37,13 @@ async fn main() -> anyhow::Result<()> {
     let region = get_bucket_region(args.inventory_base.bucket()).await?;
     let client = S3Client::new(region, args.inventory_base).await?;
     let manifest = client.get_manifest_for_date(args.date).await?;
-    let syncer = Syncer::new(client, args.outdir, args.inventory_jobs, args.object_jobs);
+    let syncer = Syncer::new(
+        client,
+        args.outdir,
+        args.inventory_jobs,
+        args.object_jobs,
+        args.path_filter,
+    );
     syncer.run(manifest).await?;
     Ok(())
 }
