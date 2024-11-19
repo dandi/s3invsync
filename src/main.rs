59ae734b548950eeeec55b3896cd7e3ead9e5ddb
John T. Wodder II <git@varonathe.org>
2024-11-19 10:42:44 -0500

    Enable tracing

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@ mod timestamps;
 use crate::s3::{get_bucket_region, S3Client, S3Location};
 use crate::syncer::Syncer;
 use crate::timestamps::DateMaybeHM;
+use anyhow::Context;
 use clap::Parser;
+use std::io::{stderr, IsTerminal};
 use std::num::NonZeroUsize;
 use std::path::PathBuf;
+use tracing::Level;
+use tracing_subscriber::{filter::Targets, fmt::time::OffsetTime, prelude::*};
 
 #[derive(Clone, Debug, Parser)]
 #[command(version)]
@@ -31,8 +35,32 @@ struct Arguments {
     outdir: PathBuf,
 }
 
+// See
+// <https://docs.rs/tracing-subscriber/latest/tracing_subscriber/fmt/time/struct.OffsetTime.html#method.local_rfc_3339>
+// for an explanation of the main + #[tokio::main]run thing
+fn main() -> anyhow::Result<()> {
+    let timer =
+        OffsetTime::local_rfc_3339().context("failed to determine local timezone offset")?;
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_timer(timer)
+                .with_ansi(stderr().is_terminal())
+                .with_writer(stderr),
+        )
+        .with(
+            Targets::new()
+                .with_target(env!("CARGO_CRATE_NAME"), Level::TRACE)
+                .with_target("aws_config", Level::DEBUG)
+                .with_target("reqwest", Level::TRACE)
+                .with_default(Level::INFO),
+        )
+        .init();
+    run()
+}
+
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn run() -> anyhow::Result<()> {
     let args = Arguments::parse();
     let region = get_bucket_region(args.inventory_base.bucket()).await?;
     let client = S3Client::new(region, args.inventory_base).await?;
